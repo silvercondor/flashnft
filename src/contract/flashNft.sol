1c77f1745640865b3cb865b39953c06e9513722b
silvercondor <biocrypto@protonmail.com>
2021-09-13 02:44:12 +0800

    fix: reduce search scope

--- src/contract/flashNft.sol
+++ src/contract/flashNft.sol
@@ -6,9 +6,10 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 contract FlashNFT {
 
   function findToken(address _addr, uint256 _start, uint256 _end) public view returns (uint256[] memory) {
+    require(_end > _start, "findToken: end < start");
     ERC721Enumerable token = ERC721Enumerable(_addr);
     uint256[] memory ids;
-    ids = new uint256[](_end);
+    ids = new uint256[](_end - _start);
     uint256 index = 0;
     for (uint256 i = _start; i < _end; i++) {
       try token.ownerOf(i) returns (address owner) {
